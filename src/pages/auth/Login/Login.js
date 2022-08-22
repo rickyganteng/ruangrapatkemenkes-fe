4c503017ecc6077f2a0ad7cfe43da10a41c93cd5
rickyganteng <syahputraricky2@gmail.com>
2022-08-22 15:00:22 +0700

    add feature hide show password login

--- src/pages/auth/Login/Login.js
+++ src/pages/auth/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import styles from "./Login.module.css";
-import { Row, Col, Button, Container, Form, Image, Card } from "react-bootstrap";
+import { Row, Col, Button, Container, Form, Image, Card, InputGroup } from "react-bootstrap";
 import { connect } from "react-redux";
 import { login } from "../../../redux/action/auth";
 import logo from "../../../assets/img/gambarkemenkes.png";
@@ -16,6 +16,7 @@ class Login extends Component {
       msg: "",
       emailValid: "valid",
       passwordValid: "valid",
+      showPassword: false,
     };
   }
 
@@ -51,6 +52,12 @@ class Login extends Component {
     this.props.history.push("/register");
   };
 
+  handleShowPass = () => {
+    this.setState({
+      showPassword: !this.state.showPassword
+    })
+  }
+
   render() {
     const {
       userName,
@@ -58,8 +65,18 @@ class Login extends Component {
       emailValid,
       passwordValid,
       msg,
+      showPassword
     } = this.state;
 
+    const iconSHowPass = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+      <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+      <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+    </svg>
+
+    const iconHidePass = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-slash-fill" viewBox="0 0 16 16">
+      <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z" />
+      <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z" />
+    </svg>
     return (
       <>
         <Container fluid>
@@ -88,13 +105,16 @@ class Login extends Component {
                     </Form.Group>
                     <Form.Group controlId="formBasicPassword">
                       <Form.Label>Password</Form.Label>
-                      <Form.Control
-                        type="password"
-                        placeholder="Password"
-                        name="userPassword"
-                        value={userPassword}
-                        onChange={(event) => this.changeText(event)}
-                      />
+                      <InputGroup>
+                        <Form.Control
+                          type={showPassword ? "text" : "password"}
+                          placeholder="Password"
+                          name="userPassword"
+                          value={userPassword}
+                          onChange={(event) => this.changeText(event)}
+                        />
+                        <InputGroup.Text onClick={this.handleShowPass}>{showPassword ? iconSHowPass : iconHidePass}</InputGroup.Text>
+                      </InputGroup>
                       <Form.Control.Feedback type={passwordValid}>
                         <p className={styles.warning}>{msg}</p>
                       </Form.Control.Feedback>
